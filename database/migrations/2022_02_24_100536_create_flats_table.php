80855d8efd27ba0975c9f5abb58af17d0872e509
MarianoPd <marianopidone@gmail.com>
2022-02-24 16:49:14 +0100

    iniziate crud e migration

---
+++ database/migrations/2022_02_24_100536_create_flats_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flats', function (Blueprint $table) {
+
+            //nullabe solo temporaneri DA RIMUOVERE
+            $table->id();
+            $table->string('title', 100);
+            $table->string('city', 45);
+            $table->string('province', 3)->nullable();
+            $table->string('address', 100)->nullable();
+            $table->string('latitude', 20)->nullable();
+            $table->string('longitude', 20)->nullable();
+            $table->tinyInteger('room_number')->unsigned()->nullable();
+            $table->tinyInteger('bed_number')->unsigned()->nullable();
+            $table->tinyInteger('bathroom_number')->unsigned()->nullable();
+            $table->smallInteger('square_meters')->unsigned()->nullable();
+            $table->string('cover')->nullable();
+            $table->string('cover_original_name')->nullable();
+            $table->boolean('visible')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flats');
+    }
+}
