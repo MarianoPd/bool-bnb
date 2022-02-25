b84b66cfc466e5060c9a3a8dd78fa24f48226229
matteofab10 <matteofabio10@gmail.com>
2022-02-25 09:57:21 +0100

    annuncio modificato con spazi

--- resources/views/host/flats/show.blade.php
+++ resources/views/host/flats/show.blade.php
@@ -4,9 +4,9 @@
     <div class="container">
         <div>
             <h3>{{$flat->title}}</h3>
-            <h5>indirizzo{{$flat->address}}</h5>
-            <h5>città{{$flat->city}}</h5>
-            <h5>provincia{{$flat->province}}</h5>
+            <h5>indirizzo: {{$flat->address}}</h5>
+            <h5>città: {{$flat->city}}</h5>
+            <h5>provincia: {{$flat->province}}</h5>
             <h5>numero di stanze: {{$flat->room_number}}</h5>
         </div>
     </div>
