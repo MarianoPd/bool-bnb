8414464a53b69122f435e08418d5dc2e64ddb1e1
Mariano Pidone <marianopidone@gmail.com>
2022-03-01 14:54:32 +0100

    guest sistemato

--- resources/views/layouts/guest_app.blade.php
+++ resources/views/layouts/guest_app.blade.php
@@ -8,44 +8,7 @@
     <link rel="stylesheet" href="{{asset('css/guest/style.css')}}">
 </head>
 <body>
-    <header class="container-fluid d-flex align-items-center">
-        <div class="container">
-            <div class="row justify-content-between align-items-baseline">
-                <div class="logo d-flex justify-content-center align-items-center">
-                    <a href="#">
-                        <span>BoolBnB</span>
-                    </a>
-                </div>
-                <div class="d-flex">
-                    <a class="{{ Request::url() === "http://127.0.0.1:8000" ? 'active' : '' }}"  href="http://127.0.0.1:8000/">
-                        
-                        <h6>Home</h6>
-                    </a>
-                    <a class="{{ Request::url() === "http://127.0.0.1:8000/appartamenti"? 'active' : '' }} mx-5"  href="http://127.0.0.1:8000/appartamenti">
-                        <h6>Flats</h6>
-                    </a>
-                    <a class="{{ Request::url() === "http://127.0.0.1:8000/chi-siamo"? 'active' : '' }}"  href="http://127.0.0.1:8000/chi-siamo">
-                        <h6>About</h6>
-                    </a>
-                </div>
-                <div>
-                    @if (Route::has('login'))
-                    <div class="top-right links">
-                        @auth
-                            <a href="{{ url('/host') }}">Home</a>
-                        @else
-                            <a href="{{ route('login') }}">Diventa Host</a>
-
-                            {{-- @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Registrati</a>
-                            @endif    --}}
-                        @endauth
-                    </div>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </header>
+    
     @yield('guest_content')
 </body>
 </html>
