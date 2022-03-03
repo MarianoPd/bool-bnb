2cbdfa164368271534e2ed3a72ddab10048dca06
Davide Tavella <tavella01@gmail.com>
2022-03-04 00:36:08 +0100

    stampa dei flat dinamica ma ERRORE FLAT.COVER

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,74 +1,26 @@
 <template>
   <section class="card">
     <div class="container">
-      <div class="row">
-        <div class="col-12 border-bottom">
-          <h1 class="my-3">I nostri Appartamenti</h1>
-        </div>
-      </div>
-      <div class="row mt-5 border-bottom">
-        <div class="col-3 border">
-          <div class="py-2">Check-In</div>
-        </div>
-        <div class="col-3 border">
-          <div class="py-2">Check-Out</div>
-        </div>
-        <div class="col-3 border">
-          <div class="py-2">Persone</div>
-        </div>
-        <div class="col-3 btn btn-outline-dark border">
-          <div class="py-2">
-            Cerca
-          </div>
-        </div>
-      </div>
 
-      <div class="row border-bottom mt-5">
+      <div 
+        v-for="(flat, index) in flats" :key="index"
+        class="row border-bottom mt-5">
+      
+        <!-- v-for="flat in flats" :key="`categ${flat.id}`" -->
+
         <div class="col-4">
           <div class="">
-            <img src="https://static.wixstatic.com/media/fde015_6c05c2f649564c37a7b5acfd3a848e5b.jpg/v1/fill/w_240,h_170,q_85,usm_0.66_1.00_0.01/fde015_6c05c2f649564c37a7b5acfd3a848e5b.jpg" alt="">
+            <img :src="flat.cover" alt="Foto non disponibile">
           </div>
-          
         </div>
-        <div class="col-4">
-          <h2>Junior Suite</h2>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor itaque voluptates beatae adipisci quibusdam minima hic sint. Possimus sunt saepe obcaecati quasi earum optio, illum at, voluptatem blanditiis magnam inventore?</p>
-        </div>
-        <div class="col-4 d-flex flex-column align-items-center">
-          <h2>A partire da</h2>
-          <h2>250$</h2>
-          <div @click="isActive = true" class="btn btn-outline-dark">More Info</div>
-        </div>
-      </div>
 
-      <div class="row border-bottom mt-5">
         <div class="col-4">
-          <div class="">
-            <img src="https://static.wixstatic.com/media/fde015_597c7d9710e44a9d91514e0ac84fb653.jpg/v1/fill/w_240,h_170,q_85,usm_0.66_1.00_0.01/fde015_597c7d9710e44a9d91514e0ac84fb653.jpg" alt="">
-          </div>
-          
-        </div>
-        <div class="col-4">
-          <h2>Junior Suite</h2>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor itaque voluptates beatae adipisci quibusdam minima hic sint. Possimus sunt saepe obcaecati quasi earum optio, illum at, voluptatem blanditiis magnam inventore?</p>
-        </div>
-        <div class="col-4 d-flex flex-column align-items-center">
-          <h2>A partire da</h2>
-          <h2>250$</h2>
-          <div @click="isActive = true" class="btn btn-outline-dark">More Info</div>
-        </div>
-      </div>
+          <h2>{{flat.title}}</h2>
 
-      <div class="row border-bottom mt-5">
-        <div class="col-4">
-          <div class="">
-            <img src="https://static.wixstatic.com/media/fde015_6159dcce72024a789a3df21e95e8d495.jpg/v1/fill/w_240,h_170,q_85,usm_0.66_1.00_0.01/fde015_6159dcce72024a789a3df21e95e8d495.jpg" alt="">
-          </div>
-          
-        </div>
-        <div class="col-4">
-          <h2>Junior Suite</h2>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor itaque voluptates beatae adipisci quibusdam minima hic sint. Possimus sunt saepe obcaecati quasi earum optio, illum at, voluptatem blanditiis magnam inventore?</p>
+          <p>
+            {{flat.address}}, {{flat.city}}, {{flat.province}}
+          </p>
+
         </div>
         <div class="col-4 d-flex flex-column align-items-center">
           <h2>A partire da</h2>
@@ -96,6 +48,9 @@ export default {
   components: {
     Popup
   },
+  props:{
+    'flats': Array
+  },
   data(){
     return{
       isActive: false,
@@ -105,6 +60,9 @@ export default {
    notActive(){
      this.isActive = false;
    }
+  },
+  computed:{
+
   }
 }
 </script>
@@ -117,5 +75,9 @@ export default {
 .active{
     display: block;
   }
+
+.card{
+  border: none;
+}
 
 </style>
